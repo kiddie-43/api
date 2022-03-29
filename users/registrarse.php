40f1f0a456d76667d4af49fbed990b410e46a3d6
edbuleo <edbuleo@ESPC-GRM-EDBULE>
2022-03-29 13:51:09 +0200

    add validaciones nick, email, telefono

--- users/registrarse.php
+++ users/registrarse.php
@@ -6,63 +6,22 @@ include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/routs.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/helpers.inc.php';
 
 $json = file_get_contents('php://input');
-
 $params = json_decode($json);
 
-// obtener datos de angular 
-$nick_user                  = html ( $params->nickUser         ); 
-$nombre_user                = html ( $params->nombreUser       ); 
-$primer_apellido_user       = html ( $params->primer_apellido  );
-$segundo_apellido_user      = html ( $params->segundo_apellido );
-$telefono_user              = html ( $params->telefono         );
-$user_email                 = html ( $params->emailUser       );
-$pass_user                  = html ( $params->passUser         );
-$confirmar_pass_user        = html ( $params->confirmarPassUser  );
 
-// $nick_user                  =  $params->nickUser     ;     
-// $nombre_user                =  $params->nombreUser  ;      
-// $primer_apellido_user       =  $params->primer_apellido  ;
-// $segundo_apellido_user      =  $params->segundo_apellido ;
-// $telefono_user              =  $params->telefono         ;
-// $user_email                 =  $params->emailUser       ;
-// $pass_user                  =  $params->passUser         ;
-// $confirmar_pass_user        =  $params->confirmarPassUser  ;
-// $registrado ; 
-// // devolvera al cliente si esta registrado o no ( devolvera el id,  un booleano y un error )
-// $resultado  ;
 
 // ver si la contraseñas son iguales 
 
-    // Ver si esta registrado antes 
-
-    // try {
-
-    //     // conexion base de datos
-    //         include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/db.inc.php';
-    //     // Montar query
-    //         $query = "SELECT COUNT(*) as estado FROM users where email_user = :email ";
-        
-    //         // Realizar peticion al server
-    //         $result = $pdo->prepare($query);
-        
-    //         $result->bindValue(":email", $user_email);
-           
-    //         $result->execute();
-            
-    //         foreach ($result as $row) {
-    //             $registrado =  ($row['estado'] !== 0 ) ? true : false;
-    //         }
-                
+if (isset($_GET["comprobar_nick"])){
     
-    //     } catch (PDOException $e) {
-    //         echo json_encode('Unable to connect to the database server.');
-    //     }
+    combrobarNick ( html($_GET["comprobar_nick"]) );
 
+} else if (isset($_GET["comprobar_email"])){
 
-if ( $registrado ){
-
-    $resultado  = [ 'error' => "El usuario ya esta registrado" ];
-    //echo json_encode($resultado);
+} else if (isset($_GET["comprobar_telefono"])){
+    combrobartelefono( html( $_GET["comprobar_telefono"] ) );
+} else {
+    registrar($params);
 }
 
 
@@ -71,40 +30,190 @@ if ( $registrado ){
 
 
 
+
+
+
+
+
+
+/*
+   
+    nick registrado 
+    correo registrado 
+    numero de telefono registrado 
+
+
+
+
+
+*/
+
+
+function combrobartelefono ( $telefono ){
+    
+    $respuesta;
+    $temporal; 
+    
+    try {
+        // conexion base de datos
+        include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/db.inc.php';
+        // Montar query
+        $query = "SELECT count(*) as registrado from users where telefono_user = :telefono";
+    
+       
+        $result = $pdo->prepare($query); 
+        $result->bindValue(':telefono', $telefono);      
+        $result->execute();
+       
+        foreach($result as $row) {
+            $temporal = ($row['registrado'] != 0 ) ? true : false ;       
+        }
+   
+        
+    
+    } catch (PDOException $e) { }
+
+if ($temporal ){
+
+$respuesta = [
+    'id' => -1, 
+    'error' => 'El telefono introducido no esta disponible'
+];
+
+echo json_encode($respuesta);
+}
+}
+
+function combrobarEmail ( $email ){
+    
+    $respuesta;
+    $temporal; 
+    
     try {
 
-    // conexion base de datos
+        // conexion base de datos
         include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/db.inc.php';
-    // Montar query
-// "    INSERT INTO `users`(`nombre_usuario`, `nombre_publico`, `primerape_usuario`, `segundoape_usuario`, `telefono_user`, `email_user`, `pass`) VALUES ('$nombre_user', '$nick_user', '$primer_apellido_user', '$segundo_apellido_user', '$telefono_user' , '$user_email', '$pass_user' )"
-// $query = "INSERT INTO `users`(`nombre_usuario`, `nombre_publico`, `primerape_usuario`, `segundoape_usuario`, `telefono_user`, `email_user`, `pass`) VALUES ($nombre_user, $nick_user, $primer_apellido_user,$segundo_apellido_user, $telefono_user , $user_email, $pass_user )";    
-$query = "    INSERT INTO `users`(`nombre_usuario`, `nombre_publico`, `primerape_usuario`, `segundoape_usuario`, `telefono_user`, `email_user`, `pass`) VALUES ('$nombre_user', '$nick_user', '$primer_apellido_user', '$segundo_apellido_user', '$telefono_user' , '$user_email', '$pass_user' )";
-
+       
+        // Montar query
+        $query = "SELECT count(*) as registrado from users where email_user = :email";
+       
+        $result = $pdo->prepare($query); 
+        $result->bindValue(':email', $email);      
+        $result->execute();
+       
+        foreach($result as $row) {
+            $temporal = ($row['registrado'] != 0 ) ? true : false ;       
+        }
+         
+            
+            
+            
     
+    } catch (PDOException $e) { }
+
+if ($temporal ){
+
+$respuesta = [
+    'id' => -1, 
+    'error' => 'El correo introducido no esta disponible'
+];
+
+echo json_encode($respuesta);
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+function combrobarNick ( $nick ){
+    
+    $respuesta;
+    $temporal; 
+    
+    try {
+
+        // conexion base de datos
+        include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/db.inc.php';
+        // Montar query
+        $query = "SELECT count(*) as registrado from users where nombre_publico = :nick";
+    
+       
+        $result = $pdo->prepare($query); 
+        $result->bindValue(':nick', $nick);      
+        $result->execute();
+       
+        foreach($result as $row) {
+            $temporal = ($row['registrado'] != 0 ) ? true : false ;       
+        }
+         
+            
+            
+            
+    
+    } catch (PDOException $e) { }
+
+if ($temporal ){
+
+$respuesta = [
+    'id' => -1, 
+    'error' => 'El nick introducido no esta disponible'
+];
+
+echo json_encode($respuesta);
+}
+
+
+}
+
+
+function registrar($params){
+  
+    
+
+    // obtener datos de angular 
+    $nick_user                  = html ( $params->nickUser         ); 
+    $nombre_user                = html ( $params->nombreUser       ); 
+    $primer_apellido_user       = html ( $params->primer_apellido  );
+    $segundo_apellido_user      = html ( $params->segundo_apellido );
+    $telefono_user              = html ( $params->telefono         );
+    $user_email                 = html ( $params->emailUser       );
+    $pass_user                  = html ( $params->passUser         );
+    $confirmar_pass_user        = html ( $params->confirmarPassUser  );
+    
+    try {
+
+        // conexion base de datos
+        include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/db.inc.php';
+        // Montar query
+        $query = "INSERT INTO `users`(`nombre_usuario`, `nombre_publico`, `primerape_usuario`, `segundoape_usuario`, `telefono_user`, `email_user`, `pass`) VALUES ('$nombre_user', '$nick_user', '$primer_apellido_user', '$segundo_apellido_user', '$telefono_user' , '$user_email', '$pass_user' )";
+    
+       
         // Realizar peticion al server
         $result = $pdo->prepare($query); 
-       
-        // :nombre,:nick,:primero,:segundo,:telefono,:email,:pass
-       
-       
-        // $result->bindValue(":nombre", $nombre_user);
-        
-        // $result->bindValue(":nick", $nick_user);
-        // $result->bindValue(":primero", $primer_apellido_user);
-        // $result->bindValue(":segundo", $segundo_apellido_user);      
-
-        // $result->bindValue(":telefono", $telefono_user);     
-        // $result->bindValue(":email", $user_email);
-        // $result->bindValue(":pass", $pass_user);
-        
-        $result->execute();
-     
-    } catch (PDOException $e) {
-       
-       // echo json_encode('Unable to connect to the database server.');    
+           
+            
+            $result->execute();
+            $id = $pdo->lastInsertId();
+         
+            $data = [
+                'id'    =>  $id, 
+                'error' =>   ''
+            ];
+            
+            echo json_encode( $data );
+    
+        } catch (PDOException $e) {
+               
     }
-
-
+    
+}
 
 
 
