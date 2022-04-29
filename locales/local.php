9260ec5b3e861760ccc09414fdf5efc9934d684e
edbuleo <edbuleo@ESPC-GRM-EDBULE>
2022-04-29 13:26:44 +0200

    add funcion de obtener localidad en local aparte

--- locales/local.php
+++ locales/local.php
@@ -30,17 +30,12 @@ try {
 foreach ($result as $row) {
     $datos = array(
         'id_local'  => $row['id_local'],
-        'direccion' =>   $row['direccion'],
+        'direccion' =>   obtenerDireccion($row['id_local']),
         'nombre_local' =>   $row['nombre_local'],
         'descripcion' =>  obtenerDescripciones( $row['id_local']),
         'carrusel' => carruselImagenes( $row['id_local'] )
     );
 }
-
-
-
-
-
 
 echo json_encode($datos);
 
@@ -67,11 +62,30 @@ function obtenerDescripciones( $id_local ){
 }
 
 
-function obtenerDireccion() {
+function obtenerDireccion( $id_local ) {
+     $data = [];
+    try {
+        include $_SERVER['DOCUMENT_ROOT'] . '/api/includes/db.inc.php';
 
-/* SELECT ciudades.nombre_ciudad, comunidadautonoma.nombre_comunidad FROM comunidadautonoma, ciudades WHERE comunidadautonoma.id_comunidad = ciudades.id_ciudad and ciudades.id_ciudad = (
-SELECT id_ciudad FROM locales WHERE locales.id_local = 2
-) 
-/*
+        $query = "SELECT ciudades.nombre_ciudad, comunidadautonoma.nombre_comunidad, locales.id_gerente FROM comunidadautonoma inner JOIN ciudades on comunidadautonoma.id_comunidad = ciudades.id_ciudad INNER JOIN locales on ciudades.id_ciudad = locales.id_ciudad WHERE locales.id_local = :id;";
+        $result = $pdo->prepare($query);
+        $result->bindValue(':id', $id_local);
+
+        $result->execute();
+    } catch (PDOException $e) {
+        $error = 'Unable to connect to the database server.';
+    }
+
+    foreach ($result as $row){
+            $data =[ 'localidad'=>$row['nombre_ciudad'] , 'comunidadAutonoma' => $row['nombre_comunidad']  ] ;
+        }
+
+    return $data;
+/* 
+SELECT ciudades.nombre_ciudad, comunidadautonoma.nombre_comunidad FROM comunidadautonoma, ciudades WHERE comunidadautonoma.id_comunidad = ciudades.id_ciudad and ciudades.id_ciudad = (SELECT id_ciudad FROM locales WHERE locales.id_local = 2 ) ;
+
+SELECT ciudades.nombre_ciudad, comunidadautonoma.nombre_comunidad, locales.id_gerente FROM comunidadautonoma inner JOIN ciudades on comunidadautonoma.id_comunidad = ciudades.id_ciudad INNER JOIN locales on ciudades.id_ciudad = locales.id_ciudad WHERE locales.id_local = 2;
+
+*/
 
 }
